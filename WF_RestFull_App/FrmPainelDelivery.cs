0c24a95e7bef41a96cbee31930a046f0527a63c5
andersonyx@hotmail.com <andersonyx@hotmail.com>
2021-01-05 11:08:32 -0300

    teste

--- WF_RestFull_App/FrmPainelDelivery.cs
+++ WF_RestFull_App/FrmPainelDelivery.cs
@@ -4,12 +4,6 @@ using Application.Services.Domain.Standard;
 using Application.Services.Interface.Standard;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WF_RestFull_App
