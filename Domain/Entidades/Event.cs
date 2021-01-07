d81dae04452d2e9f4c1bd60cbf5e3691808a368a
andersonyx@hotmail.com <andersonyx@hotmail.com>
2021-01-07 09:56:38 -0300

    Inciando a criação das tabelas de banco de dados

---
+++ Domain/Entidades/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Entidades
+{
+    [Table("TblEvent")]
+    public class Event
+    {
+        [Key]
+        [Display(Name = "Id#")]
+        public int EventId { get; set; }
+    }
+}
---
+++ Domain/Entidades/EventPolling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Entidades
+{
+    [Table("TblEventPolling")]
+    public class EventPolling
+    {
+        [Key]
+        [Display(Name = "Id#")]
+        public int EventPollingId { get; set; }
+
+        [Required(ErrorMessage = "Informe o conteúdo do arquivo JSON.")]
+        [Display(Name = "Dados do arquivo JSON")]
+        public string ConteudoJSON { get; set; }
+
+        [Key]
+        [Display(Name = "Quantidade de eventos no arquivo JSON")]
+        public int QuantidadeEventos { get; set; }
+
+        [Display(Name = "Data de cadastro")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+
+        [Required]
+        [Display(Name = "Acknowledged")]
+        public bool Acknowledged { get; set; } = false;
+    }
+}
+
+
+
